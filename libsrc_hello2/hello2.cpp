76f277a4663351d62adc912fd5c68e541f46b4a0
boomboomgzy <3228352301@qq.com>
2023-09-01 23:51:29 +0800

    update gitignore file

---
+++ libsrc_hello2/hello2.cpp
@@ -0,0 +1,7 @@
+#include "../include/hello2.h"
+#include<iostream>
+
+void SayHello2() {
+    std::cout<<"hello2 say "<<std::endl;
+}
+
---
+++ src/hello.cpp
@@ -0,0 +1,6 @@
+#include "../include/hello.h"
+#include<iostream>
+
+void SayHello() {
+    std::cout<<"hello say "<<std::endl;
+}
